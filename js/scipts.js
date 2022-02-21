cdd4928565cbdc026abd64fd891f385a7763ee65
Todd Pangilinan <pang.todd@gmail.com>
2022-02-20 16:01:14 -0800

    changed branching logic to case statements.

--- js/scipts.js
+++ js/scipts.js
@@ -1,19 +1,26 @@
 // business logic
 
 
-function Player(firstName, score) {
+function Player(firstName, score, turn) {
   this.firstName = firstName;
   this.score = score;
-}
+  this.turn = turn;
+
 Player.score = 0
 
 function rollDice() {
   roll = Math.floor(Math.random() * 6) + 1;
-    if (roll === 1) { 
-      Player.score = 0    
-    } else { 
-      Player.score= (Player.score + roll)
-    }
+
+  switch(roll) {
+    case('1',1):
+      Player.score = 0;
+      Player.turn = false;
+      break;
+    default:
+      Player.score=(Player.score + roll)
+      break;
+  }
+  
   console.log("current roll: "+roll)
   console.log("overall Score: "+Player.score)
 }
