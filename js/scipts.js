d9f1ef93ce0aa120f39bafc451801326bac79eeb
michaelshamrell <michaelshamrell2001@gmail.com>
2022-02-23 11:13:26 -0800

    Create changeimage() that changes image of dice depending on the dice roll.

--- js/scipts.js
+++ js/scipts.js
@@ -1,97 +1,132 @@
-///PLAYER 1 ----------------------
+///PLAYER 1: Grabbing ID from html and turning them into variables------------
 var rollP1El = document.getElementById("rollP1-el")
 var turnScoreP1El = document.getElementById("turnScoreP1-el")
 var totalScoreP1El = document.getElementById("totalScoreP1-el")
-////PLAYER 2 ---------------------
+////PLAYER 2: Grabbing ID from html and turning them into variables-----------
 var rollP2El = document.getElementById("rollP2-el")
 var turnScoreP2El = document.getElementById("turnScoreP2-el")
 var totalScoreP2El = document.getElementById("totalScoreP2-el")
-//PLAYER 1 OBJECT -------------
+//PLAYER 1 OBJECT ------------------------------------------------------------
 /// Refactor object code 
-//create a win fucntion
-function Player1(score, turnScore, totalScore) {
-  this.score = score;
+function Player1(turnScore, totalScore) {
   this.turnScore = turnScore;
   this.totalScore = totalScore;
 }
-Player1.score = 0
+//Defining Player1 variables -------------------------------------------------
 Player1.totalScore = 0
-//PLAYER 2 OBJECT -------------
-function Player2(score, turnScore, totalScore) {
-  this.score = score;
+Player1.turnScore = 0
+//PLAYER 2 OBJECT ------------------------------------------------------------
+function Player2(turnScore, totalScore) {
   this.turnScore = turnScore;
   this.totalScore = totalScore;
 }
-Player2.score = 0
+//Defining Player2 variables--------------------------------------------------
 Player2.totalScore = 0
-//Disable Function---------------------------
-function disableP1(){
+Player2.turnScore = 0
+//Disable and Enable Button Function------------------------------------------
+function disableP1EnableP2(){
   document.getElementById("p2Roll").disabled = false;
   document.getElementById("p2Save").disabled = false; 
   document.getElementById("p1Roll").disabled = true;
   document.getElementById("p1Save").disabled = true;
 }
-function disableP2(){
+function disableP2EnableP1(){
   document.getElementById("p2Roll").disabled = true;
   document.getElementById("p2Save").disabled = true; 
   document.getElementById("p1Roll").disabled = false;
   document.getElementById("p1Save").disabled = false;
 }
-// ROLL DICE P1---------------
+//WIN CHECK FUNCTION---------------------------------------------------------------
+function winCheck() {
+  if (Player1.totalScore + Player1.turnScore >= 30 ) {
+    document.getElementById("game").style.display = "none";
+    document.getElementById("p1Win").style.display = "block";
+  } else if (Player2.totalScore + Player2.turnScore >= 30) {
+    document.getElementById("game").style.display = "none";
+    document.getElementById("p2Win").style.display = "block";
+  }
+}
+///CHANGE IMAGE LAYER 1 ----------------------------------------------------------
+function changeImageP1() {
+  var p1Image = document.getElementById("p1Image");
+  if (p1Roll === 1) {
+    p1Image.src = "img/1P1.jpg";
+  } else if ( p1Roll === 2) {
+    p1Image.src = "img/2P1.jpg";
+  }  else if ( p1Roll === 3) {
+    p1Image.src = "img/3P1.jpg";
+  } else if ( p1Roll === 4) {
+    p1Image.src = "img/4P1.jpg";
+  } else if ( p1Roll === 5) {
+    p1Image.src = "img/5P1.jpg";
+  } else if ( p1Roll === 6) {
+    p1Image.src = "img/6P1.jpg";
+  }  
+}
+//CHANGE IMAGE PLAYER 2 -----------------------------------------------------------
+function changeImageP2() {
+  var p2Image = document.getElementById("p2Image");
+  if ( p2Roll === 1) {
+    p2Image.src = "img/1P2.jpg";
+  }  else if ( p2Roll === 2) {
+    p2Image.src = "img/2P2.jpg";
+  } else if ( p2Roll === 3) {
+    p2Image.src = "img/3P2.jpg";
+  } else if ( p2Roll === 4) {
+    p2Image.src = "img/4P2.jpg";
+  } else if ( p2Roll === 5) {
+    p2Image.src = "img/5P2.jpg";
+  }  else if ( p2Roll === 6) {
+    p2Image.src = "img/6P2.jpg";
+  } 
+}
+
+
+// ROLL DICE P1--------------------------------------------------------------
 function rollDiceP1() {
   p1Roll = Math.floor(Math.random() * 6) + 1;
-    if (p1Roll === 1) { 
-      Player1.score = 0 
-      Player1.turnScore = 0 
-      disableP1()
-    } else { 
-      Player1.score = (Player1.score + p1Roll)
-      Player1.turnScore = (Player1.score)
-    }
-  rollP1El.textContent = "current roll: " + p1Roll
-  turnScoreP1El.textContent = "Score: " + Player1.score
+      if (p1Roll === 1) { 
+        Player1.turnScore = 0 
+        disableP1EnableP2()
+      } else { 
+        Player1.turnScore = Player1.turnScore + p1Roll
+      }
+    rollP1El.textContent = "Current Dice Roll: " + p1Roll
+    turnScoreP1El.textContent = "Turn Score: " + Player1.turnScore
+    changeImageP1()
+    winCheck()
 }
-// ROLL DICE P2---------------
+// ROLL DICE P2--------------------------------------------------------------
 function rollDiceP2() {
   p2Roll = Math.floor(Math.random() * 6) + 1;
     if (p2Roll === 1) { 
-      Player2.score = 0 
       Player2.turnScore = 0 
-      disableP2()  
+      disableP2EnableP1()  
     } else { 
-      Player2.score = (Player2.score + p2Roll)
-      Player2.turnScore = (Player2.score)
+      Player2.turnScore = (Player2.turnScore + p2Roll)
     }
-  rollP2El.textContent = "current roll: " + p2Roll
-  turnScoreP2El.textContent = "Score: " + Player2.score
+  rollP2El.textContent = "Current Dice Roll: " + p2Roll
+  turnScoreP2El.textContent = "Turn Score: " + Player2.turnScore
+  changeImageP2()
+  winCheck()
 }
-//SAVE ROLL P1 --------------
-//take math out of save function
+//SAVE ROLL P1 --------------------------------------------------------------
 function saveRollP1() {
   Player1.totalScore = Player1.totalScore + Player1.turnScore
   totalScoreP1El.textContent = "Total Score: " + Player1.totalScore
-    disableP1()
-    Player1.score = 0
-    Player1.turnScore = 0
-    if (Player1.totalScore + Player1.turnScore >= 10 ) {
-      console.log("Player  1 wins")
-    }
+  Player1.turnScore = 0
+  disableP1EnableP2()
 }
 
-//SAVE ROLL P2 --------------
+//SAVE ROLL P2 --------------------------------------------------------------
 function saveRollP2() {
   Player2.totalScore = Player2.totalScore + Player2.turnScore
   totalScoreP2El.textContent = "Total Score: " + Player2.totalScore
-    disableP2()
-      Player2.score = 0
-      Player2.turnScore = 0
-      //create a win function
-      if (Player2.totalScore + Player2.turnScore >= 10) {
-        console.log("Player  2 wins")
-      }
+  Player2.turnScore = 0
+    disableP2EnableP1()
 }
  
-//Setting the Player 1 as the starting player
+//Setting the Player 1 as the starting player--------------------------------
 $(document).ready(function() {
   document.getElementById("p2Roll").disabled = true;
   document.getElementById("p2Save").disabled = true;
